207918b1261a776268acb150d8d938a7a8b2af25
unknown <m.borgaonker313@gmail.com>
2024-02-16 14:47:51 +0530

    updated package.json

--- src/sections/Faculty.jsx
+++ src/sections/Faculty.jsx
@@ -6,7 +6,7 @@ export default function Faculty() {
   const [isPending, setIsPending] = useState(true);
 
   useEffect(()=>{
-    fetch('https://mru-new-7hhf.vercel.app/blogs')
+    fetch('http://localhost:8000/blogs')
     .then(res => {
       return res.json();
     })
